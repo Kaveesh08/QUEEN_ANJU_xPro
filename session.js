7219dd48793f0390e4ed3e67f4381af351130396
Kaveesh08 <kaveeshapromod0808@gmail.com>
2025-02-06 19:45:54 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~2MdH3QSC#VwuNDgjF_VjerzzkwcdiNb7cYfMXCaCvmvSBGT48uDQ",
 PORT: process.env.PORT || "8000"
 };
